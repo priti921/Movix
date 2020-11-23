b260ea0a80c8aed79494628ead714801882619c6
Priti <roypriti921@gmail.com>
2020-11-23 13:52:27 +0530

    working on styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,8 +3,15 @@ import Headroom from 'react-headroom';
 
 
 const navbar = () => {
-    return <Headroom>
-        <h1>Movix</h1>
+    return <Headroom className="Headroom">
+        <p className="Movix">Movix</p>
+        <input className="search" type="text" />
+        <ul className="nav">
+            <li>Home</li>
+            <li>Movies</li>
+            <li>Series</li>
+            <li>My list</li>
+        </ul>
     </Headroom>;
 }
 
