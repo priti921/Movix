decb4aaeb587e0ee0e9867f99e4d086e61ac716e
Priti <roypriti921@gmail.com>
2020-11-29 13:26:06 +0530

    working on movieSlider

--- src/components/MovieSlider.js
+++ src/components/MovieSlider.js
@@ -4,7 +4,7 @@ import Axios from 'axios';
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 function MovieSlider() {
-    // api key 
+
     const [Data, setData] = useState([]);
 
 
@@ -12,6 +12,7 @@ function MovieSlider() {
         const fetchData = async () => {
             await Axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${Key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`)
                 .then((res) => setData(res.data.results.slice(0, 10)))
+                .then(() => console.log(Data))
                 .catch((err) => console.error(err))
         }
         fetchData();
@@ -22,14 +23,21 @@ function MovieSlider() {
         Data.map((movie) => {
             return (
                 <div className="movie" key={movie.id}>
-                    <li className="movie-title">{movie.title}</li>
-                    <img className="movie-poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
+                    <img className="movie-poster" src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} />
+                    <ul className="movie-details">
+                        <li>{movie.title}</li>
+                        <li>{movie.overview}</li>
+                        <li>{movie.popularity}</li>
+                        <li>{movie.vote_average}</li>
+                        <li>{movie.release_date.slice(0, 4)}</li>
+                        <li>{movie.genre_ids}</li>
+                    </ul>
                 </div>
             )
         })
     ) : (<p>Loading..</p>)
     return (
-        <AwesomeSlider>
+        <AwesomeSlider className="slider">
             {topMovies}
         </AwesomeSlider>
     )
