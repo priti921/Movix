cc8d0b8653faf0335c0c5219f3011b8d87825e72
Priti <roypriti921@gmail.com>
2020-12-17 01:47:43 +0530

    added series page

--- src/page/Series.js
+++ src/page/Series.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import PopularSeries from '../components/Series/PopularSeries'
+import TrendingSeries from '../components/Series/TrendingSeries'
+import TopratedSeries from '../components/Series/TopratedSeries'
+import AiringSeries from '../components/Series/AiringSeries';
+import About from '../components/about';
 
 function Series() {
     return (
         <div className="series-page">
-            Series here
+            <span className="black-box"></span>
+            <PopularSeries />
+            <TrendingSeries />
+            <AiringSeries />
+            <TopratedSeries />
+
+            <About />
         </div>
     )
 }
